c6d1c4d74997c83b18e9a964e2d9f853ba6533a4
rugero fabris <frugero@mail.com>
2021-02-10 18:37:25 -0500

    continued working on LabTest1 ( implemented MongoDb and AXIOS )

--- LabTest1/client/src/components/Chat/Chat.jsx
+++ LabTest1/client/src/components/Chat/Chat.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
-import queryString from 'query-string';
+
 import io from "socket.io-client";
-
-import TextContainer from '../TextContainer/TextContainer';
-import Messages from '../Messages/Messages';
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
-
-import './chat.css';
+import queryString from "query-string";
+import React, { useState, useEffect } from "react";
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import axios from "axios";
+import "./chat.css";
 
 // const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
-const ENDPOINT = 'localhost:5000/';
+const ENDPOINT = "http://localhost:5000/";
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
@@ -26,43 +26,52 @@ const Chat = ({ location }) => {
     socket = io(ENDPOINT);
 
     setRoom(room);
-    setName(name)
+    setName(name);
 
-    socket.emit('join', { name, room }, (error) => {
-      if(error) {
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
         alert(error);
+      } else {
+        axios.get(`${ENDPOINT}chats/rooms/${room}`).then((res) => {
+          // console.log(res.data);
+          setMessages(res.data);
+        });
       }
     });
   }, [location.search]);
-  
+
   useEffect(() => {
-    socket.on('message', message => {
-      setMessages(messages => [ ...messages, message ]);
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
     });
-    
+
     socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
-}, []);
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
 
-    if(message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
     }
-  }
+  };
 
   return (
     <div className="outerContainer">
       <div className="container">
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
-      <TextContainer users={users}/>
+      <TextContainer users={users} />
     </div>
   );
-}
+};
 
 export default Chat;
--- LabTest1/server/index.js
+++ LabTest1/server/index.js
@@ -1,11 +1,11 @@
+const cors = require("cors");
 const http = require("http");
 const express = require("express");
-const socketio = require("socket.io");
-const cors = require("cors");
-
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
-
 const router = require("./router");
+const chatModel = require("./chat");
+const mongoose = require("mongoose");
+const socketio = require("socket.io");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(
@@ -15,7 +15,18 @@ const io = socketio(
     origins: ["http://localhost:3000"],
   })
 );
-
+mongoose.connect(
+  "mongodb+srv://RFabris:mC1q7m12D2PObVHC@cluster0.lw10r.mongodb.net/gbc_full_stack?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
 app.use(cors());
 app.use(router);
 
@@ -31,6 +42,7 @@ io.on("connect", (socket) => {
       user: "admin",
       text: `${user.name}, welcome to room ${user.room}.`,
     });
+
     socket.broadcast
       .to(user.room)
       .emit("message", { user: "admin", text: `${user.name} has joined!` });
@@ -46,8 +58,11 @@ io.on("connect", (socket) => {
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit("message", { user: user.name, text: message });
-
+    io.to(user.room).emit(
+      "message",
+      saveChat(user.room, { user: user.name, text: message })
+    );
+    console.log("save message to database");
     callback();
   });
 
@@ -55,10 +70,13 @@ io.on("connect", (socket) => {
     const user = removeUser(socket.id);
 
     if (user) {
-      io.to(user.room).emit("message", {
-        user: "Admin",
-        text: `${user.name} has left.`,
-      });
+      io.to(user.room).emit(
+        "message",
+        saveChat(user.room, {
+          user: "Admin",
+          text: `${user.name} has left.`,
+        })
+      );
       io.to(user.room).emit("roomData", {
         room: user.room,
         users: getUsersInRoom(user.room),
@@ -70,3 +88,23 @@ io.on("connect", (socket) => {
 server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
 );
+
+const saveChat = (room, message) => {
+  const chatMessage = new chatModel({ room, ...message });
+  try {
+    chatMessage.save((err) => {
+      if (err) {
+        console.log(err.errors);
+      } else {
+        console.log("message saved to DB");
+      }
+    });
+  } catch (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("message saved to DB");
+    }
+  }
+  return message;
+};
